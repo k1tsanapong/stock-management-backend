3e23301e0020a7f24ce13f554d4e818960aed2c7
Seki Darubi <6431503002@lamduan.mfu.ac.th>
2022-11-24 09:13:35 +0700

    last_monk

--- db/index.js
+++ db/index.js
@@ -5,22 +5,44 @@ const mysql = require(`mysql-await`);
 const db = knex.default({
     client: 'mysql2',
     connection: {  
-        user: 'root',
+        host: 'mysql-sour20.alwaysdata.net',
+        user: 'sour20',
         password: 'kitsanapong',
-        host: '127.0.0.1',
-        port: 3306,
-        database: 'dbsour20'
+        database: 'sour20_stocker',
+        // port: 3306,
     }
 })
 
 
 const conn = mysql.createConnection({
-    host: "127.0.0.1",
-    user: "kim",
-    password: "kim",
-    database: "dbsour20",
-    port:3306
+    host: "mysql-sour20.alwaysdata.net",
+    user: "sour20",
+    password: "kitsanapong",
+    database: "sour20_stocker",
+    // port:3306
   });
+
+
+//   const db = knex.default({
+//     client: 'mysql2',
+//     connection: {  
+//         user: 'root',
+//         password: 'kitsanapong',
+//         host: '127.0.0.1',
+//         port: 3306,
+//         database: 'dbsour20'
+//     }
+// })
+
+
+// const conn = mysql.createConnection({
+//     host: "127.0.0.1",
+//     user: "kim",
+//     password: "kim",
+//     database: "dbsour20",
+//     port:3306
+//   });
+
 
 module.exports = {
     db,
